e0dbcb1fc791a28174940c8f0873c0ad4bf9edba
bruhfl <40370653+bruhfl@users.noreply.github.com>
2021-01-13 18:02:31 -0300

    Add files via upload
    Primeiro programa em cobol, famoso Hello World

---
+++ PROGCOB01.cob
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+      *****************************************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR = BRUNO LIMA - BFL
+      * OBJETIVO: MOSTRAR O HELLO WORLD
+      * DATA: XX/XX/XXXX
+      *****************************************************************
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+            DISPLAY 'HELLO 2021'.
+            STOP RUN.
